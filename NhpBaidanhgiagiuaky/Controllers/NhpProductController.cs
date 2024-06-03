0dc928dde79e92fa0c283e0984153560ba3b84f7
Nguyễn Hữu Phúc <nguyenphuc10122003@gmail.com>
2024-06-03 11:13:35 +0700

    Initial commit

---
+++ NhpBaidanhgiagiuaky/Controllers/NhpProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NhpBaidanhgiagiuaky.Models;
+
+namespace NhpBaidanhgiagiuaky.Controllers
+{
+    public class NhpProductController : Controller
+    {
+        private static List<NhpProductController> NhpProducts = new List<NhpProductController>()
+{
+             new NhpProductController{nhpId=204, nhpName="Banh Bao", nhpImage="../Images/Anhbanhbao.png", nhpQuantity=24,nhpPrice=10000,nhpIsActive=true},
+             new NhpProductController{nhpId=202, nhpName="Banh Ca", nhpImage="../Images/Anhbanhca.png", nhpQuantity=14,nhpPrice=12000,nhpIsActive=true}
+};
+        // GET: NhpProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Get: Create Products
+        public ActionResult NhpCreate()
+        {
+            var NhpProduct = new NhpProductController ();
+            return View(NhpProduct);
+        }
+
+        //Post: Create Products
+        [HttpPost]
+        public ActionResult NhpCreate(NhpProductController NhpProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(NhpProduct);
+            }
+
+            NhpProduct.Add(NhpProduct);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NhpBaidanhgiagiuaky/Models/NhpProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace NhpBaidanhgiagiuaky.Models
+{
+    public class NhpProduct
+    {
+        [Required(ErrorMessage = "Hãy nhập mã số")]
+        public int nhpId { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên sản phẩm")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Tên sản phẩm phải dài từ 5 đến 100 ký tự")]
+        [RegularExpression("[A-Z ]+", ErrorMessage = "Tên sản phẩm chỉ chứa ký tự viết hoa và khoảng trắng")]
+        public string nhpName { get; set; }
+        [Required(ErrorMessage = "Hãy nhập ảnh")]
+        public string nhpImage { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập số lượng sản phẩm")]
+        [Range(1, 100, ErrorMessage = "Số lượng sản phẩm phải nằm trong khoảng từ 1 đến 100")]
+        public int nhpQuantity { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập giá sản phẩm")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Giá sản phẩm phải lớn hơn 0")]
+        public decimal nhpPrice { get; set; }
+        public bool nhpIsActive { get; set; } = true;
+    }
+}
